46df14511c1e9f83f90c4f4b8b702f08b97ce9bf
julian zulatto <julianzulatto14@outlook.com>
2023-10-10 14:28:01 -0300

    Iniciamos el repo con el archivo receta1.txt

---
+++ prueba_git.py
@@ -0,0 +1,59 @@
+# clase 16 de git
+
+#siempre que se trabaje con git, crear siempre carpetas por separadas y nunca poner una dentro de otra
+
+#al estar el archivo color verde quiere decir que git lo esta reconociendo!
+
+#git init (comando para iniciar un repositorio .git)
+#git status (para ver como estan los archivos en el repositorio (area de trabajo, o guardados, etc))
+#git add (sirve para pasar del working directoy (area de trabajo)al de staging area(donde guardo lo q esta
+# mas o menos bien))
+#git add receta1.txt (pasa de tener una U a tener un A y cambia de color)
+#git config --global user.name
+#git config --global user.email
+#git commit -m 'iniciamos el repo con el archivo receta1.txt'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
